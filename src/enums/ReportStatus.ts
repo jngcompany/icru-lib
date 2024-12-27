e0990e7e9cf61178d2b02732ae6603b96b4d6521
Asanobm <asanobm@outlook.com>
2024-12-27 15:54:29 +0900

    add Report

---
+++ src/enums/ReportStatus.ts
@@ -0,0 +1,10 @@
+/*
+ * SPDX-FileCopyrightText: © 2024, 주식회사 지음과깃듬 <jngcompany.co.kr | asanobm@outlook.com>
+ * SPDX-License-Identifier: UNLICENSED
+ */
+
+export enum ReportStatus {
+  PENDING = 'PENDING',
+  COMPLETED = 'COMPLETED',
+  FAILED = 'FAILED',
+}
---
+++ src/interfaces/Report.ts
@@ -0,0 +1,17 @@
+/*
+ * SPDX-FileCopyrightText: © 2024, 주식회사 지음과깃듬 <jngcompany.co.kr | asanobm@outlook.com>
+ * SPDX-License-Identifier: UNLICENSED
+ */
+
+
+import {ReportStatus} from "../enums/ReportStatus";
+
+export interface Report {
+  id: string;
+  userId: string;
+  diagnosisId: string;
+  organizationId: string;
+  debrieferId: string;
+  downloadUrl: string;
+  status: ReportStatus;
+}
