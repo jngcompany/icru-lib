335ea119c789415e10de999e8bcd4ee26d0e6fb3
Asanobm <asanobm@outlook.com>
2025-02-11 15:05:31 +0900

    chore: bump version to 0.4.1 and update Diagnosis interface with permanentDiagnosis property

--- src/interfaces/Diagnosis.ts
+++ src/interfaces/Diagnosis.ts
@@ -11,40 +11,28 @@ import { UserAnswer } from '../types/UserAnswer'
 import { Debriefer } from './Debriefer'
 import { DiagnosisQuestion } from './DiagnosisQuestion'
 import { Invitation } from './Invitation'
+import { PermanentDiagnosis } from './PermanentDiagnosis'
 import { User } from './User'
 
 /**
- * 진단 정보를 나타내는 인터페이스입니다.
- *
+ * 진단 인터페이스
  * @interface Diagnosis
- * @extends DocumentData
- *
- * @property {string} id - 진단의 고유 식별자입니다.
- * @property {User} [user] - 진단을 받은 사용자 정보입니다.
- * @property {boolean} isAnonymous - 익명 여부를 나타냅니다.
- * @property {boolean} isFirst - 첫 번째 진단 여부를 나타냅니다.
- * @property {Debriefer} [debriefer] - 디브리퍼 정보입니다.
- * @property {DiagnosisStatus} status - 진단 상태를 나타냅니다.
- * @property {Object} scores - 진단 점수입니다.
- * @property {Object} scores.TEMPERAMENT - 기질 점수입니다.
- * @property {number} scores.TEMPERAMENT.R - 기질 점수 R입니다.
- * @property {number} scores.TEMPERAMENT.E - 기질 점수 E입니다.
- * @property {number} scores.TEMPERAMENT.A - 기질 점수 A입니다.
- * @property {number} scores.TEMPERAMENT.D - 기질 점수 D입니다.
- * @property {Object} scores.RESILIENCE - 회복력 점수입니다.
- * @property {number} scores.RESILIENCE.R - 회복력 점수 R입니다.
- * @property {number} scores.RESILIENCE.E - 회복력 점수 E입니다.
- * @property {number} scores.RESILIENCE.A - 회복력 점수 A입니다.
- * @property {number} scores.RESILIENCE.D - 회복력 점수 D입니다.
- * @property {Object} [case] - 진단 케이스 정보입니다.
- * @property {MainType} case.mainType - 주요 유형입니다.
- * @property {RevolutionerSubType | EmotionerSubType | ActionerSubType | DetectorSubType} case.subType - 하위 유형입니다.
- * @property {(Array<{ id: string; step: number }> | Question[])} [questions] - 질문 목록입니다.
- * @property {UserAnswer[]} [answers] - 사용자 답변 목록입니다.
- * @property {Invitation} [invitation] - 초대 정보입니다.
- * @property {Timestamp} createdAt - 생성 일시입니다.
- * @property {Timestamp} updatedAt - 수정 일시입니다.
- * @property {(Timestamp | null)} [deletedAt] - 삭제 일시입니다.
+ * @extends {DocumentData}
+ * @property {string} id - 진단의 고유 식별자.
+ * @property {User} user - 진단을 수행한 사용자.
+ * @property {boolean} isAnonymous - 익명 진단 여부.
+ * @property {boolean} isFirst - 첫 번째 진단 여부.
+ * @property {Debriefer} debriefer - 디브리퍼.
+ * @property {DiagnosisStatus} status - 진단 상태.
+ * @property {{ TEMPERAMENT: { R: number; E: number; A: number; D: number }; RESILIENCE: { R: number; E: number; A: number; D: number }}} scores - 진단 점수.
+ * @property {{ mainType: MainType; subType: RevolutionerSubType | EmotionerSubType | ActionerSubType | DetectorSubType }} case - 진단 케이스.
+ * @property {DiagnosisQuestion[] | { id: string; step: number }[]} questions - 질문 목록.
+ * @property {UserAnswer[]} answers - 사용자 답변 목록.
+ * @property {Invitation} invitation - 초대장.
+ * @property {PermanentDiagnosis} permanentDiagnosis - 영구 진단.
+ * @property {Timestamp} createdAt - 생성일.
+ * @property {Timestamp} updatedAt - 수정일.
+ * @property {Timestamp | null} deletedAt - 삭제일.
  */
 export interface Diagnosis extends DocumentData {
   id: string
@@ -74,6 +62,7 @@ export interface Diagnosis extends DocumentData {
   questions?: { id: string; step: number }[] | DiagnosisQuestion[]
   answers?: UserAnswer[]
   invitation?: Invitation
+  permanentDiagnosis?: PermanentDiagnosis
   createdAt: Timestamp
   updatedAt: Timestamp
   deletedAt?: Timestamp | null
